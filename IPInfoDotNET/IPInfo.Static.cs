237432766b565a6b9a4e4d507fda8d1a46a0008d
Unreal852 <dodo-pl-08@hotmail.fr>
2017-04-08 14:28:47 +0200

    Doc

--- IPInfoDotNET/IPInfo.Static.cs
+++ IPInfoDotNET/IPInfo.Static.cs
@@ -6,26 +6,51 @@ namespace IPInfoDotNET
 {
     public partial class IPInfo
     {
+        /// <summary>
+        /// Get the IP information of the current machine ( public IP )
+        /// </summary>
+        /// <returns><see cref="IPInfo"/> Result</returns>
         public static IPInfo Get()
         {
             return Get(null);
         }
 
+        /// <summary>
+        /// Get the IP information of the given IP
+        /// </summary>
+        /// <param name="ip">IP to check</param>
+        /// <returns><see cref="IPInfo"/> Result</returns>
         public static IPInfo Get(string ip)
         {
             return new IPInfo(GetParserRequest(ip));
         }
 
+        /// <summary>
+        /// Get a specific IP information of the current machine
+        /// </summary>
+        /// <param name="key"><see cref="IPInfoKey"/> Value to get</param>
+        /// <returns>Result</returns>
         public static string Get(IPInfoKey key)
         {
             return GetRequest(key, null);
         }
 
+        /// <summary>
+        /// Get the specific IP information of the given IP
+        /// </summary>
+        /// <param name="key"><see cref="IPInfoKey"/> Value to get</param>
+        /// <param name="ip">IP to check</param>
+        /// <returns>Result</returns>
         public static string Get(IPInfoKey key, string ip)
         {
             return GetRequest(key, ip);
         }
 
+        /// <summary>
+        /// Get request for the given ip
+        /// </summary>
+        /// <param name="ip">IP</param>
+        /// <returns>Response</returns>
         private static string GetRequest(string ip)
         {
             WebRequest request = WebRequest.Create(ip == null ? "http://ipinfo.io/" : "http://ipinfo.io/" + ip);
@@ -34,6 +59,12 @@ namespace IPInfoDotNET
             return reader.ReadToEnd();
         }
 
+        /// <summary>
+        /// Get request for the given key and ip
+        /// </summary>
+        /// <param name="key"><see cref="IPInfoKey"/> Value to get</param>
+        /// <param name="ip">IP</param>
+        /// <returns>Response</returns>
         private static string GetRequest(IPInfoKey key, string ip)
         {
             WebRequest request = WebRequest.Create(ip == null ? "http://ipinfo.io/" + key.ToString().ToLower() : "http://ipinfo.io/" + ip + "/" + key.ToString().ToLower());
@@ -42,12 +73,22 @@ namespace IPInfoDotNET
             return reader.ReadToEnd();
         }
 
+        /// <summary>
+        /// Get a request and parse it
+        /// </summary>
+        /// <param name="ip">IP</param>
+        /// <returns>Parser</returns>
         private static JSParser GetParserRequest(string ip)
         {
             string json = GetRequest(ip);
             return new JSParser(json);
         }
 
+        /// <summary>
+        /// Download data from the given URL ( used for downloading google map location )
+        /// </summary>
+        /// <param name="url">Url to download the file from</param>
+        /// <returns>Downloaded Data</returns>
         private static byte[] DownloadData(string url)
         {
             byte[] downloadedData = new byte[0];
@@ -64,7 +105,6 @@ namespace IPInfoDotNET
                 while (true)
                 {
                     int bytesRead = stream.Read(buffer, 0, buffer.Length);
-
                     if (bytesRead == 0)
                         break;
                     else
